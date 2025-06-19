6acd0599f05b298ec1738db1d13164cc08a1dbcc
Yusi <yusi4781@naver.com>
2025-06-19 17:20:29 +0900

    feat: TopBar 레이아웃 스타일 수정 및 검색 기능 추가

--- src/components/MobileTopBar.tsx
+++ src/components/MobileTopBar.tsx
@@ -1,10 +1,40 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { FormEvent, useState } from "react";
 import { FaSearch } from "react-icons/fa";
 
 export default function MobileTopBar() {
+  const [searchText, setSearchText] = useState("");
+  const router = useRouter();
+
+  const handleSearch = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!searchText.trim()) return;
+
+    router.push(`/search?params=${encodeURIComponent(searchText)}`);
+  };
+
   return (
-    <div className="fixed top-0 left-0 right-0 bg-[#6CC2FF] text-white h-16 flex justify-between items-center z-50">
-      <img src="/goorm_logo_white.png" alt="로고" className="w-14 ml-3" />
-      <FaSearch size={20} className="mr-3" />
+    <div className="fixed top-0 left-0 right-0 bg-[#6CC2FF] h-16 flex justify-between items-center px-4 z-50">
+      <img src="/goorm_logo_white.png" alt="로고" className="w-12 h-12 mr-3" />
+
+      <form
+        className="flex items-center bg-white rounded-full px-4 py-2 w-full max-w-[550px]"
+        onSubmit={handleSearch}
+      >
+        <input
+          type="text"
+          placeholder="검색"
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+          className="flex-grow outline-none text-black bg-transparent placeholder-gray-400"
+        />
+        <button type="submit">
+          <FaSearch className="text-[#6CC2FF]" />
+        </button>
+      </form>
     </div>
   );
 }
