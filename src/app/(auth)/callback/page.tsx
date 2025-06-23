1eec95365fa41c9b65bff5ee3ca5922a96972b88
Yusi <yusi4781@naver.com>
2025-06-23 18:11:43 +0900

    fix: 배포 오류 수정

--- src/app/(auth)/callback/page.tsx
+++ src/app/(auth)/callback/page.tsx
@@ -1,18 +1,25 @@
 "use client";
 
 import { useSearchParams } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, Suspense } from "react";
 
-export default function Callback() {
+function InnerCallback() {
   const params = useSearchParams();
   const code = params.get("code");
 
   useEffect(() => {
     if (code) {
-      console.log("✅ Spotify 로그인 성공, code:", code);
-      // 여기서 백엔드로 code 보내서 access_token 요청
+      console.log("✅ code:", code);
     }
   }, [code]);
 
-  return <div>Spotify 로그인 중...</div>;
+  return <div>Spotify 로그인 처리 중...</div>;
+}
+
+export default function Page() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <InnerCallback />
+    </Suspense>
+  );
 }
