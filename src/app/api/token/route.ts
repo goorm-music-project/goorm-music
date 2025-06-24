d6c425343a42ed952b0684fd1c77e68bbbb6ed0a
Yusi <yusi4781@naver.com>
2025-06-24 09:59:56 +0900

    feat: token api 작성

---
+++ src/app/api/token/route.ts
@@ -0,0 +1,29 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+  const code = body.code;
+
+  const client_id = process.env.SPOTIFY_CLIENT_ID!;
+  const client_secret = process.env.SPOTIFY_CLIENT_SECRET!;
+  const redirect_uri = "http://127.0.0.1:3000/callback";
+
+  const params = new URLSearchParams();
+  params.append("grant_type", "authorization_code");
+  params.append("code", code);
+  params.append("redirect_uri", redirect_uri);
+  params.append("client_id", client_id);
+  params.append("client_secret", client_secret);
+
+  const response = await fetch("https://accounts.spotify.com/api/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: params.toString(),
+  });
+
+  const data = await response.json();
+
+  return NextResponse.json(data);
+}
