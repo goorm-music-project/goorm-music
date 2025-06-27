34fdc50a092056acbbeb68b40d5585f8e8a09fba
Yusi <yusi4781@naver.com>
2025-06-27 09:43:58 +0900

    feat: 로그인 상태 확인 전역 상태로 변경

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,15 +1,12 @@
-import { cookies } from "next/headers";
 import NewReleaseAlbums from "../../domains/main/components/NewReleaseAlbums";
 import RandomRecoList from "../../domains/main/components/RandomRecoList";
 import TopChartList from "../../domains/main/components/TopChartList";
 import JenreRecoList from "@/domains/main/components/JenreRecoList";
 import LikedList from "@/domains/main/components/LikedList";
+import { userSpotifyStore } from "@/domains/common/stores/userSpotifyStore";
 
 export default async function page() {
-  //임시
-  const cookieStore = await cookies();
-  const userId = cookieStore.get("access_token")?.value;
-  const isLoggedIn = !!userId;
+  const { isLoggedIn } = userSpotifyStore.getState();
 
   return (
     <div className="">
