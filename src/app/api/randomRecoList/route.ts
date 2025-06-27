aba931ae0e48b2dc42487d737302af543da083b5
Yusi <yusi4781@naver.com>
2025-06-27 09:34:42 +0900

    refactor: 필요없는 주석 삭제

--- src/app/api/randomRecoList/route.ts
+++ src/app/api/randomRecoList/route.ts
@@ -1,18 +1,12 @@
 import { getAccessToken } from "@/domains/common/lib/getAccessToken";
 import axios from "axios";
-// import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
 export async function GET() {
   try {
-    // const cookieAwait = await cookies();
-    // const token = cookieAwait.get("access_token")?.value;
     const token = await getAccessToken();
 
-    // 임시 : 개인 플레이리스트 출력
     const playlistId = "48AlewkJlCLDasGjfzcAoB";
-    // const playlistId = "37i9dQZEVXbMDoHDwVN2tF";
-    // const playlistId = "37i9dQZF1DX3rxVfibe1L0";
     const res = await axios.get(
       `https://api.spotify.com/v1/playlists/${playlistId}/tracks?limit=5`,
       {
