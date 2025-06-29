2a7a99334888fc17d4ceafad90ee04d1488f3e3a
김소연 <ksy9252@naver.com>
2025-06-29 20:07:46 +0900

    feat : 장르 추천 API 연결

---
+++ src/app/api/jenreRecoList/route.ts
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+
+export async function GET(req: Request) {
+  const cookieAwait = await cookies();
+  const access_token = cookieAwait.get("access_token")?.value;
+  const { searchParams } = new URL(req.url);
+  const jenre = searchParams.get("genre") || "pop";
+
+  try {
+    const res = await axios.get(
+      `https://api.spotify.com/v1/search?q=genre:${jenre}&type=track&limit=10`,
+      {
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+        },
+      }
+    );
+    const data = await res.data.tracks.items;
+    return NextResponse.json(data);
+  } catch (err) {
+    console.log("장르 리스트 오류", err);
+    return NextResponse.json(err);
+  }
+}
