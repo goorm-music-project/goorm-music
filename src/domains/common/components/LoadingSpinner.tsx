733d3be6c42120eddbaf5bfeeb0675abe072fde2
Yusi <yusi4781@naver.com>
2025-07-01 13:04:57 +0900

    feat: 로딩 스피너 부모 컴포넌트 중앙에 오도록 수정

--- src/domains/common/components/LoadingSpinner.tsx
+++ src/domains/common/components/LoadingSpinner.tsx
@@ -1,5 +1,7 @@
 export default function LoadingSpinner() {
   return (
-    <div className="animate-spin rounded-full h-16 w-16 border-b-2 border-blue-500" />
+    <div className="absolute inset-0 flex items-center justify-center">
+      <div className="animate-spin rounded-full h-16 w-16 border-b-2 border-blue-500 mb-16" />
+    </div>
   );
 }
