cafd8cd275355fef61a08d26734dbbc68530bad0
Yusi <yusi4781@naver.com>
2025-07-01 17:44:06 +0900

    feat: 곡 상세 페이지에서 해당 아티스트의 인기 많은 곡 리스트를 추가

--- src/app/(main)/track/[id]/page.tsx
+++ src/app/(main)/track/[id]/page.tsx
@@ -4,13 +4,10 @@ import LoadingSpinner from "@/domains/common/components/LoadingSpinner";
 import Image from "next/image";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
-import { HiOutlineDotsVertical } from "react-icons/hi";
-import { FaPlay } from "react-icons/fa";
-import { AiOutlineLike } from "react-icons/ai";
-import Link from "next/link";
 import TrackActionBtns from "@/domains/track/components/TrackActionBtns";
 import TrackInfo from "@/domains/track/components/TrackInfo";
 import TrackLyrics from "@/domains/track/components/TrackLyrics";
+import ArtistMoreTracks from "@/domains/track/components/ArtistMoreTracks";
 
 interface TrackDetail {
   artists: string[]; // 아티스트 이름
@@ -50,12 +47,15 @@ export default function TrackDetailPage() {
   if (error || !track) return <div>오류 발생: {error}</div>;
 
   return (
-    <div className="flex flex-col items-center">
-      <Image src={track.imageUrl} alt="곡 사진" width={300} height={300} />
-      <TrackActionBtns />
-      <h1 className="mt-4">{track.title}</h1>
-      <TrackInfo artists={track.artists} artistsId={track.artistsId} />
-      <TrackLyrics lyrics={track.lyrics} />
+    <div>
+      <div className="flex flex-col items-center">
+        <Image src={track.imageUrl} alt="곡 사진" width={300} height={300} />
+        <TrackActionBtns />
+        <h1 className="mt-4">{track.title}</h1>
+        <TrackInfo artists={track.artists} artistsId={track.artistsId} />
+        <TrackLyrics lyrics={track.lyrics} />
+      </div>
+      <ArtistMoreTracks artistId={track.artistsId[0]} />
     </div>
   );
 }
---
+++ src/domains/track/components/ArtistMoreTracks.tsx
@@ -0,0 +1,76 @@
+"use client";
+
+import LoadingSpinner from "@/domains/common/components/LoadingSpinner";
+import TrackCard from "@/domains/common/components/TrackCard";
+import { useEffect, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+
+type artistMoreTracksType = [
+  {
+    imageUrl: string;
+    title: string;
+    artists: string[];
+  }
+];
+
+export default function ArtistMoreTracks({ artistId }: { artistId: string }) {
+  const [tracks, setTracks] = useState<artistMoreTracksType | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchTracks = async () => {
+      try {
+        const res = await fetch(`/api/artist/${artistId}/top-tracks`);
+        if (!res.ok) throw new Error("아티스트 인기곡 불러오기 실패");
+        const data = await res.json();
+        setTracks(data);
+      } catch (err: any) {
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    if (artistId) {
+      fetchTracks();
+    }
+  }, [artistId]);
+
+  if (loading) return <LoadingSpinner />;
+
+  return (
+    <div>
+      <h2 className="mt-3 mb-3">이 아티스트의 인기곡들을 만나보세요.</h2>
+      {error
+        ? error
+        : tracks && (
+            <Swiper
+              slidesPerView={"auto"}
+              spaceBetween={30}
+              freeMode={true}
+              pagination={{
+                clickable: true,
+              }}
+              className="w-full"
+            >
+              {tracks.map((track) => (
+                <SwiperSlide
+                  key={track.imageUrl + track.title}
+                  className="!w-[150px]"
+                >
+                  <TrackCard
+                    imageUrl={track.imageUrl}
+                    name={track.title}
+                    artists={track.artists}
+                  />
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          )}
+    </div>
+  );
+}
