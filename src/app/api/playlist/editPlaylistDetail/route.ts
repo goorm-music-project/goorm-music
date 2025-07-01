1f141c7a01b0f78543b0cd9d3b75e6461c910861
김소연 <ksy9252@naver.com>
2025-07-01 11:11:28 +0900

    feat : 플레이리스트 정보 수정 API 연결

---
+++ src/app/api/playlist/editPlaylistDetail/route.ts
@@ -0,0 +1,29 @@
+import axios from "axios";
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+
+export async function PUT(req: Request) {
+  const cookieAwait = await cookies();
+  const access_token = cookieAwait.get("access_token")?.value;
+  const { name, description, isPublic, id } = await req.json();
+
+  try {
+    await axios.put(
+      `https://api.spotify.com/v1/playlists/${id}`,
+      {
+        name,
+        public: isPublic === "true",
+        description,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    return NextResponse.json("플레이리스트 수정 완료");
+  } catch (err) {
+    console.log("플레이리스트 수정 오류", err);
+  }
+}
