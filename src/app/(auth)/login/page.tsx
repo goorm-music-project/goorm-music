34fb72ea2baefa41fc2ddb1eabb5d049618f6f85
Yusi <yusi4781@naver.com>
2025-07-02 17:18:46 +0900

    refactor: img -> Image 태그로 변경

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import Image from "next/image";
+
 export default function Login() {
   const handleLogin = () => {
     const clientId = process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID;
@@ -19,12 +21,12 @@ export default function Login() {
 
   return (
     <div className="flex flex-col items-center">
-      <img src="/goorm_logo_blue.png" alt="로고" className="w-80 mt-10" />
+      <Image src="/goorm_logo_blue.png" alt="로고" className="mt-10" width={320} height={320}/>
       <button
         className="flex items-center border border-gray-800 px-8 py-4 rounded hover:bg-gray-100"
         onClick={handleLogin}
       >
-        <img src="/spotify_logo.png" alt="spotify logo" className="w-10 mr-4" />{" "}
+        <Image src="/spotify_logo.png" alt="spotify logo" className="mr-4" width={40} height={40}/>{" "}
         spotify로 로그인하기
       </button>
     </div>
