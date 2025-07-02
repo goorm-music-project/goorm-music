adbf79397793edb2eda3a7fad37b5664617deceb
Yusi <yusi4781@naver.com>
2025-07-02 15:27:51 +0900

    feat: 바텀 네브바 route 설정

--- src/domains/layout/components/BottomNavBar.tsx
+++ src/domains/layout/components/BottomNavBar.tsx
@@ -1,18 +1,38 @@
+"use client";
+
+import { useRouter } from "next/navigation";
 import { FaMusic, FaHome, FaUser } from "react-icons/fa";
 
 export default function BottomNavBar() {
   return (
     <nav className="fixed bottom-0 left-0 right-0 bg-(--primary-blue) text-white h-16 flex justify-around items-center z-50">
-      <NavItem icon={<FaMusic size={20} />} label="장르 검색" />
-      <NavItem icon={<FaHome size={20} />} label="홈" />
-      <NavItem icon={<FaUser size={20} />} label="마이 페이지" />
+      <NavItem icon={<FaMusic size={20} />} label="장르 검색" path="/search" />
+      <NavItem icon={<FaHome size={20} />} label="홈" path="/" />
+      <NavItem
+        icon={<FaUser size={20} />}
+        label="마이 페이지"
+        path="/profile"
+      />
     </nav>
   );
 }
 
-function NavItem({ icon, label }: { icon: React.ReactNode; label: string }) {
+function NavItem({
+  icon,
+  label,
+  path,
+}: {
+  icon: React.ReactNode;
+  label: string;
+  path: string;
+}) {
+  const router = useRouter();
+
   return (
-    <div className="flex flex-col items-center text-sm">
+    <div
+      className="flex flex-col items-center text-sm"
+      onClick={() => router.push(path)}
+    >
       {icon}
       <span className="mt-1">{label}</span>
     </div>
