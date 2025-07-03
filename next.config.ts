ecedfaf08ad54dc91c23ea9c7bf1dabf89276aa4
김소연 <ksy9252@naver.com>
2025-07-03 11:15:43 +0900

    docs : 이미지 config 추가

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["mosaic.scdn.co", "i.scdn.co"],
+    domains: ["mosaic.scdn.co", "i.scdn.co", "image-cdn-ak.spotifycdn.com"],
   },
 };
 
