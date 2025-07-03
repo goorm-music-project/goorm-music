99e8e41d295b20af1f24abb8200bb3f80b44dc41
김소연 <ksy9252@naver.com>
2025-07-03 11:16:39 +0900

    feat : 공통  alert 모달 추가

---
+++ src/domains/common/components/AlertModal.tsx
@@ -0,0 +1,24 @@
+import React, { useEffect } from "react";
+import Modal from "./Modal";
+interface Props {
+  showModal: boolean;
+  onClose: () => void;
+  message: string;
+}
+export default function AlertModal({ showModal, onClose, message }: Props) {
+  useEffect(() => {
+    if (!showModal) return;
+    const timer = setTimeout(() => {
+      onClose();
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, [showModal, onClose]);
+
+  return (
+    <Modal showModal={showModal} onClose={onClose}>
+      <div className="w-xs text-center">
+        <h3 className="text-center">{message}</h3>
+      </div>
+    </Modal>
+  );
+}
