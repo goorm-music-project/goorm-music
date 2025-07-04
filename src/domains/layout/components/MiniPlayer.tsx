8e8d1ad91ce00d67dd274741056d4612b9613aee
Yusi <yusi4781@naver.com>
2025-07-04 13:20:52 +0900

    style: 미니 플레이어 세로 크기 수정

--- src/domains/layout/components/MiniPlayer.tsx
+++ src/domains/layout/components/MiniPlayer.tsx
@@ -8,11 +8,11 @@ export default function MiniPlayer() {
   if (!selectedTrackId) return null;
 
   return (
-    <div className="fixed bottom-16 left-0 w-full z-50 bg-[#1f1f1f]">
+    <div className="fixed bottom-11 left-0 w-full z-50 bg-[#1f1f1f]">
       <iframe
         src={`https://open.spotify.com/embed/track/${selectedTrackId}?utm_source=generator&theme=0`}
         width="100%"
-        height="152"
+        height="100"
         allow="encrypted-media"
       />
     </div>
