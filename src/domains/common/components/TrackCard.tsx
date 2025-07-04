e25e4c34a31171459c1cd125247c598bb72e2fe9
Yusi <yusi4781@naver.com>
2025-07-04 13:18:29 +0900

    feat: trackCard 클릭 시 현재 재생 곡 수정

--- src/domains/common/components/TrackCard.tsx
+++ src/domains/common/components/TrackCard.tsx
@@ -4,11 +4,17 @@ interface TrackCardProps {
   imageUrl: string;
   name: string;
   artists: string[];
+  onClick: () => void;
 }
 
-export default function TrackCard({ imageUrl, name, artists }: TrackCardProps) {
+export default function TrackCard({
+  imageUrl,
+  name,
+  artists,
+  onClick,
+}: TrackCardProps) {
   return (
-    <div>
+    <div onClick={onClick}>
       <Image src={imageUrl} alt={name} width={150} height={150} />
       <p className="truncate my-1">{name}</p>
       <p className="truncate">{artists.join(", ")}</p>
