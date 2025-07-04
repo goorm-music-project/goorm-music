391bda11fec050e536ebae52eacaf8a0f9539fcd
Yusi <yusi4781@naver.com>
2025-07-04 10:53:38 +0900

    style: 하단 미니플레이어 세로 크기 변경

--- src/domains/layout/components/MiniPlayer.tsx
+++ src/domains/layout/components/MiniPlayer.tsx
@@ -8,18 +8,12 @@ export default function MiniPlayer() {
   if (!selectedTrackId) return null;
 
   return (
-    <div
-      className="fixed bottom-16 left-0 w-full z-50 bg-[#1f1f1f]"
-      style={{
-        pointerEvents: "none", // iframe 자체를 제외한 이벤트 막음
-      }}
-    >
+    <div className="fixed bottom-16 left-0 w-full z-50 bg-[#1f1f1f]">
       <iframe
         src={`https://open.spotify.com/embed/track/${selectedTrackId}?utm_source=generator&theme=0`}
         width="100%"
-        height="152" // 모바일용 작은 높이 고정
+        height="152"
         allow="encrypted-media"
-        className="pointer-events-auto" // iframe 내부는 클릭 가능
       />
     </div>
   );
