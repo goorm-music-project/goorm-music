cd7b48bba272eb7723f35294430655aa97e7536b
Yusi <yusi4781@naver.com>
2025-07-09 10:40:09 +0900

    delete: layout에서 refresh token 관련 코드 삭제

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -3,9 +3,6 @@
 import BottomNavBar from "@/domains/layout/components/BottomNavBar";
 import MobileTopBar from "@/domains/layout/components/MobileTopBar";
 import InitUserIdState from "@/domains/common/components/InitUserIdState";
-import { useEffect } from "react";
-import axios from "axios";
-import { userSpotifyStore } from "@/domains/common/stores/userSpotifyStore";
 import MiniPlayer from "@/domains/layout/components/MiniPlayer";
 import { usePlayerSotre } from "@/domains/common/stores/usePlayerStore";
 
@@ -15,27 +12,6 @@ export default function MainLayout({
   children: React.ReactNode;
 }) {
   const { selectedTrackId } = usePlayerSotre();
-
-  useEffect(() => {
-    const tryRefresh = async () => {
-      const res = await fetch("/api/refresh-token", { method: "POST" });
-
-      if (res.status === 200) {
-        const userData = await axios.post("/api/userData");
-        const { userId } = userData.data;
-
-        if (userId) {
-          userSpotifyStore.getState().setUserId(userId);
-          userSpotifyStore.getState().setIsLoggedIn(true);
-        }
-      } else {
-        userSpotifyStore.getState().setUserId("");
-        userSpotifyStore.getState().setIsLoggedIn(false);
-      }
-    };
-
-    tryRefresh();
-  }, []);
 
   return (
     <div className="min-h-screen relative">
