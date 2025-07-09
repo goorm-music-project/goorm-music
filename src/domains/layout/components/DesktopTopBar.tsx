9fa91073996ecf37ff025da52da5feb402ced34b
Yusi <yusi4781@naver.com>
2025-07-09 14:59:33 +0900

    style: 스크롤 가능한 화면에서 top bar와 겹치게 보이는 화면 수정

--- src/domains/layout/components/DesktopTopBar.tsx
+++ src/domains/layout/components/DesktopTopBar.tsx
@@ -20,7 +20,7 @@ export default function DesktopTopBar() {
   };
 
   return (
-    <div className="w-full h-25 px-4 flex items-center justify-between border-b-2 border-(--primary-blue) bg-none fixed top-0 left-0 z-50">
+    <div className="w-full h-25 px-4 flex items-center justify-between border-b-2 border-(--primary-blue) bg-(--background) fixed top-0 left-0 z-50">
       <Image
         src="/goorm_logo_blue.png"
         alt="로고"
