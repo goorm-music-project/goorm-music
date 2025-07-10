ccf342bae5b62b50cb11683121a5b6f5c6e624c3
seung <monitz@naver.com>
2025-07-10 12:58:21 +0900

    feat:미로그인 마이페이지 클릭시 로그인필요 팝업

--- src/domains/layout/components/BottomNavBar.tsx
+++ src/domains/layout/components/BottomNavBar.tsx
@@ -1,44 +1,44 @@
 "use client";
-
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 import { FaMusic, FaHome, FaUser } from "react-icons/fa";
-import { userSpotifyStore } from "@/domains/common/stores/userSpotifyStore";
+import SuggestLoginModal from "@/domains/common/components/SuggestLoginModal";
 
 export default function BottomNavBar() {
-  const userId = userSpotifyStore((state) => state.userId);
-
-  return (
-    <nav className="fixed bottom-0 left-0 right-0 bg-(--primary-blue) text-white h-16 flex justify-around items-center z-50">
-      <NavItem icon={<FaMusic size={20} />} label="장르 검색" path="/genre" />
-      <NavItem icon={<FaHome size={20} />} label="홈" path="/" />
-      {/* 여기서 path만 userId를 넣어서 전달! */}
-      <NavItem
-        icon={<FaUser size={20} />}
-        label="마이 페이지"
-        path={userId ? `/profile/${userId}` : "/auth/login"}
-      />
-    </nav>
-  );
-}
-
-function NavItem({
-  icon,
-  label,
-  path,
-}: {
-  icon: React.ReactNode;
-  label: string;
-  path: string;
-}) {
+  const [showModal, setShowModal] = useState(false);
   const router = useRouter();
 
   return (
-    <div
-      className="flex flex-col items-center text-sm cursor-pointer"
-      onClick={() => router.push(path)}
-    >
-      {icon}
-      <span className="mt-1">{label}</span>
-    </div>
+    <>
+      <nav className="fixed bottom-0 left-0 right-0 bg-(--primary-blue) text-white h-16 flex justify-around items-center z-50">
+        <div
+          className="flex flex-col items-center text-sm cursor-pointer"
+          onClick={() => router.push("/genre")}
+        >
+          <FaMusic size={20} />
+          <span className="mt-1">장르 검색</span>
+        </div>
+        <div
+          className="flex flex-col items-center text-sm cursor-pointer"
+          onClick={() => router.push("/")}
+        >
+          <FaHome size={20} />
+          <span className="mt-1">홈</span>
+        </div>
+
+        <div
+          className="flex flex-col items-center text-sm cursor-pointer"
+          onClick={() => setShowModal(true)}
+        >
+          <FaUser size={20} />
+          <span className="mt-1">마이 페이지</span>
+        </div>
+      </nav>
+
+      <SuggestLoginModal
+        showModal={showModal}
+        onClose={() => setShowModal(false)}
+      />
+    </>
   );
 }
