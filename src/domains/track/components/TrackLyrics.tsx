63fcffdbbf5ef198e8442524035932dd58759583
Yusi <yusi4781@naver.com>
2025-07-14 16:01:03 +0900

    refactor: api 분리에 따른 api 호출 방식 변경과 가사 렌더링 방식 변경

--- src/domains/track/components/TrackLyrics.tsx
+++ src/domains/track/components/TrackLyrics.tsx
@@ -1,11 +1,41 @@
+import LoadingSpinner from "@/domains/common/components/LoadingSpinner";
+import appAxios from "@/domains/common/lib/axios/appAxios";
+import { useEffect, useState } from "react";
+
 interface TrackLyricsProps {
-  lyrics: string | null;
+  title: string;
+  artist: string;
 }
 
-export default function TrackLyrics({ lyrics }: TrackLyricsProps) {
+export default function TrackLyrics({ title, artist }: TrackLyricsProps) {
+  const [lyric, setLyric] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const fetchLyrics = async () => {
+      setLoading(true);
+      try {
+        const res = await appAxios.post("/api/lyric", { title, artist });
+        setLyric(res.data.lyrics);
+      } catch (err) {
+        console.error("가사 조회 실패:", err);
+        setLyric("가사를 지원하지 않는 곡입니다.");
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchLyrics();
+  }, []);
+
   return (
-    <div className="rounded border p-5 h-75 overflow-y-auto w-full max-w-md text-sm leading-relaxed">
-      {lyrics ? lyrics : "가사를 지원하지 않는 곡입니다."}
+    <div className="rounded border p-5 h-75 overflow-y-auto w-full max-w-md text-sm leading-relaxed relative">
+      {loading ? (
+        <LoadingSpinner />
+      ) : lyric ? (
+        <pre className="whitespace-pre-wrap">{lyric}</pre>
+      ) : (
+        <div className="text-gray-500">가사를 찾을 수 없습니다.</div>
+      )}
     </div>
   );
 }
