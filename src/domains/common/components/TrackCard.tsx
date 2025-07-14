657655125fc59d79788857d5c6ad489ba42f7ab1
Yusi <yusi4781@naver.com>
2025-07-14 13:47:16 +0900

    style: cursor-pointer 설정

--- src/domains/common/components/TrackCard.tsx
+++ src/domains/common/components/TrackCard.tsx
@@ -14,7 +14,7 @@ export default function TrackCard({
   onClick,
 }: TrackCardProps) {
   return (
-    <div className="w-[150px]" onClick={onClick}>
+    <div className="w-[150px] cursor-pointer" onClick={onClick}>
       <Image src={imageUrl} alt={name} width={150} height={150} />
       <p className="truncate my-1">{name}</p>
       <p className="truncate">{artists?.join(", ")}</p>
