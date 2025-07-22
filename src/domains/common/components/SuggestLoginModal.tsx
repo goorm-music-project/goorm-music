7a7025722e94a96271203594c03053b62567a2b3
김소연 <ksy9252@naver.com>
2025-07-22 10:46:15 +0900

    fix: 로그인 유도 모달 여백 수정

--- src/domains/common/components/SuggestLoginModal.tsx
+++ src/domains/common/components/SuggestLoginModal.tsx
@@ -9,10 +9,10 @@ interface Props {
 export default function SuggestLoginModal({ showModal, onClose }: Props) {
   return (
     <Modal showModal={showModal} onClose={onClose}>
-      <div className="text-center">
-        <h1 className="text-center mt-3">로그인 후 이용해주세요.</h1>
+      <div className="text-center w-[300px]">
+        <h1 className="text-center mt-4">로그인 후 이용해주세요.</h1>
         <Link href={"/login"}>
-          <button className="primaryBtn p-2 mt-5">로그인하기</button>
+          <button className="primaryBtn p-2 my-4">로그인하기</button>
         </Link>
       </div>
     </Modal>
