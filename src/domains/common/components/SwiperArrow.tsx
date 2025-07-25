ec2e71b04a29b028f2af916172c5ca2f71c14007
김소연 <ksy9252@naver.com>
2025-07-25 17:20:38 +0900

    fix : 인디케이터 높이 수정

--- src/domains/common/components/SwiperArrow.tsx
+++ src/domains/common/components/SwiperArrow.tsx
@@ -6,14 +6,14 @@ export default function SwiperArrow({ top = "45%" }: { top?: string }) {
     <>
       <div
         className={`custom-prev absolute ${
-          top === "0" ? "top-0" : `top-[${top}]`
+          top === "0" ? "top-0" : `top-[45%]`
         } left-1 z-10 cursor-pointer text-3xl`}
       >
         <MdArrowBackIos />
       </div>
       <div
         className={`custom-next absolute ${
-          top === "0" ? "top-0" : `top-[${top}]`
+          top === "0" ? "top-0" : `top-[45%]`
         }  right-0 z-10 cursor-pointer text-3xl`}
       >
         <MdArrowForwardIos />
