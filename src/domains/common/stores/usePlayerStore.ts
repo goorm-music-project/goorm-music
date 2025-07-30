31c6f36cbad1b461df33716b9d196617b500c281
Yusi <yusi4781@naver.com>
2025-07-31 01:14:55 +0900

    fix: 미니 플레이어 새로 고침 시 사라지는 버그 수정

--- src/domains/common/stores/usePlayerStore.ts
+++ src/domains/common/stores/usePlayerStore.ts
@@ -1,11 +1,22 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 interface PlayerState {
   selectedTrackId: string | null;
   setSelectedTrackId: (id: string) => void;
 }
 
-export const usePlayerStore = create<PlayerState>((set) => ({
-  selectedTrackId: null,
-  setSelectedTrackId: (id) => set({ selectedTrackId: id }),
-}));
+export const usePlayerStore = create<PlayerState>()(
+  persist(
+    (set) => ({
+      selectedTrackId: null,
+      setSelectedTrackId: (id) => set({ selectedTrackId: id }),
+    }),
+    {
+      name: "player-storage",
+      partialize: (state) => ({
+        selectedTrackId: state.selectedTrackId,
+      }),
+    }
+  )
+);
