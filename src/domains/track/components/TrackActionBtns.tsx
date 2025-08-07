a0dc0cb0c358b83db88793f3d71ba3e616e3e4fc
Yusi <yusi4781@naver.com>
2025-08-07 14:54:09 +0900

    fix: 좋아요 상태 버그 수정

--- src/domains/track/components/TrackActionBtns.tsx
+++ src/domains/track/components/TrackActionBtns.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import LikedButton from "@/domains/common/components/LikedButton";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { userSpotifyStore } from "@/domains/common/stores/userSpotifyStore";
 import { FaPlus } from "react-icons/fa";
 import PlayListModal from "@/domains/playlist/components/PlayListModal";
@@ -9,13 +9,14 @@ import { Playlist } from "@/domains/playlist/types/Playlist";
 import AddNewPlayListModal from "@/domains/playlist/components/AddNewPlayListModal";
 import AlertModal from "@/domains/common/components/AlertModal";
 import SuggestLoginModal from "@/domains/common/components/SuggestLoginModal";
+import authAxios from "@/domains/common/lib/axios/authAxios";
 
 interface TrackActionBtnsProps {
   trackId: string;
 }
 
 export default function TrackActionBtns({ trackId }: TrackActionBtnsProps) {
-  const [isLiked, setIsLiked] = useState(false);
+  const [isLiked, setIsLiked] = useState<boolean>(false);
   const [showPlayListModal, setShowPlayListModal] = useState<boolean>(false);
   const [showAddNewPlayListModal, setShowAddNewPlayListModal] = useState(false);
   const [playlists, setPlaylists] = useState<Playlist[]>([]);
@@ -24,6 +25,20 @@ export default function TrackActionBtns({ trackId }: TrackActionBtnsProps) {
   const [message, setMessage] = useState("");
   const [showAlertModal, setShowAlertModal] = useState(false);
   const [showLoginModal, setShowLoginModal] = useState(false);
+
+  const fetchIsLiked = async () => {
+    try {
+      const res = await authAxios.get(`/api/isLiked?trackId=${trackId}`);
+      const result = res.data.data[0] as boolean;
+      setIsLiked(result);
+    } catch (err) {
+      console.error("좋아요 상태 가져오기 실패", err);
+    }
+  };
+
+  useEffect(() => {
+    fetchIsLiked();
+  }, []);
 
   return (
     <div className="relative flex flex-col items-center mt-4">
